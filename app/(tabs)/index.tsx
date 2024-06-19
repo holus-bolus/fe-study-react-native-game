065f333b573e1a1a557f9695e34d1850027ff274
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-06-19 10:05:01 +0300

    Added the bg image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,24 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "@/app/screens/StartGameScreen";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.rootScreen}>
-      <StartGameScreen></StartGameScreen>
-    </View>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("../../assets/images/bg-image.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={{ opacity: 0.5 }}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
   },
 });
