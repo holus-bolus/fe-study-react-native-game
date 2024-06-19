fbaaaad403e3dd37c73072ef267f742468d1588b
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-06-19 08:28:27 +0300

    Added the buttons styling

--- app/components/PrimaryButton.tsx
+++ app/components/PrimaryButton.tsx
@@ -1,12 +1,46 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 import { ReactNode } from "react";
 
 const PrimaryButton = ({ children }: { children: ReactNode }) => {
+  const pressHandler = () => {
+    console.log("PrimaryButton pressed");
+  };
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressed, styles.buttonInnerContainer]
+            : styles.buttonInnerContainer
+        }
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#ddb52f",
+    alignItems: "center",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "#ffffff",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
